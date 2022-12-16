3fefd3a98dd2bb2a70bc4fb8b60153ce4d3fda62
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-12-16 12:56:28 +0200

    added get_prediction, get_labels_score functions

--- app/Utils/model_explain/exp_lime.py
+++ app/Utils/model_explain/exp_lime.py
@@ -1,6 +1,8 @@
 """This module for model explainabilty using lime"""
 
 from lime.lime_text import LimeTextExplainer
+import numpy as np
+import tensorflow as tf
 
 class explainer():
     def __init__(self,model_predictor):
@@ -14,9 +16,65 @@ class explainer():
         self.explainer = LimeTextExplainer(class_names=self.class_names)
 
     def explain_texts(self,text,top_labels=None):
-        num_feature=50
-        exp = self.explainer.explain_instance(text,
+        #TODO handle Id
+        num_feature=20
+        self.exp = self.explainer.explain_instance(text,
                     self.model_predictor.predict_explain,
+                    labels=range(len(self.class_names)),
                     num_features=num_feature,
                     top_labels=top_labels)
-        return exp
+        return self.exp
+
+    def get_prediction(self):
+        #TODO catch exception if didn't call explain_texts() first
+        self.index = np.argmax(self.exp.predict_proba)
+        prediction = self.class_names[self.index]
+        return prediction
+    
+
+    # TODO a function that gets all labels with their score
+    def get_labels_score(self):
+        output = {}
+        
+        labels_with_score = {}
+        predic_proba = self.exp.predict_proba
+        for indx,label in enumerate(self.class_names):
+            labels_with_score[label] = predic_proba[indx]
+
+
+        output["scores"] = labels_with_score
+        return output
+    # TODO a function that uses exp.get_list(self.index) to get names of the word
+
+    # TODO a function that gets words place exp.as_map()[self.index]
+
+    # TODO a function that combine them all together and return:
+    '''{
+    "predictions":[
+        {
+            "Id":3245,
+            "label":"ham",
+            "scores":{
+                "ham": score,
+                "spam":score
+                },
+            "explanations":{
+                "token1":{
+                    "position":"index",
+                    "score":score
+                },
+
+                "token2":{
+                    "position":"index",
+                    "score":score
+                }
+            }
+            }
+        ]
+    }
+    '''
+
+
+
+
+    
